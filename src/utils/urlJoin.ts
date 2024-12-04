2465ae69401f05398b3c38d15c11a2bf78149069
Parker Watkins <parkergw@gmail.com>
2024-12-04 14:48:22 -0700

    url encode arguments to fix spaces.

--- src/utils/urlJoin.ts
+++ src/utils/urlJoin.ts
@@ -3,6 +3,6 @@ import { join } from "path"
 import env from "../config/env"
 
 export default function urlJoin(apiKey: string, ...args: string[]): string {
-  const url = join("api", ...args, `?apiKey=${apiKey}`)
+  const url = join("api", encodeURI(join(...args)), `?apiKey=${apiKey}`)
   return `${env.remoteUrl}/${url}`
 }
