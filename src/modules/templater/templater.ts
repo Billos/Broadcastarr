ef429856c9cf2427d152b85181a3d23b9cef870b
billos <billos@billos.fr>
2025-01-19 23:47:33 +0100

    feat(templater): enhance TemplateRenderer with date formatting and error handling

--- src/modules/templater/templater.ts
+++ src/modules/templater/templater.ts
@@ -1,7 +1,52 @@
-import nunjucks from "nunjucks"
+import { DateTime } from "luxon"
+import { Environment } from "nunjucks"
 
-export class TemplateRenderer extends nunjucks.Environment {
+import mainLogger from "../../utils/logger"
+
+export class TemplateRenderer extends Environment {
   constructor() {
-    super()
+    super(null, { autoescape: false, throwOnUndefined: true })
+
+    this.addGlobal("dateToFormat", (days: number, format: string) => {
+      const date = DateTime.local().plus({ days })
+      return date.toFormat(format)
+    })
+
+    this.addFilter("formatDate", (date: string, inputFormat: string, outputFormat: string) => {
+      const forbiddenFormats = ["x", "X"]
+      if (forbiddenFormats.includes(inputFormat)) {
+        throw new Error(`Invalid input format: ${inputFormat}`)
+      }
+
+      const parsedDate = DateTime.fromFormat(date, inputFormat)
+
+      if (!parsedDate.isValid) {
+        throw new Error(`Invalid date ${date} with format ${inputFormat}`)
+      }
+      return parsedDate.toFormat(outputFormat)
+    })
+
+    this.addGlobal("now", (format: string) => DateTime.local().toFormat(format))
+  }
+
+  override renderString(template: string, context: Record<string, any>): string {
+    const logger = mainLogger.getSubLogger({ name: "TemplateRenderer", prefix: ["renderString"] })
+    const templateRegex = /({{.*}})|({%.*%})/g
+    if (!template || !template.match(templateRegex)) {
+      return template
+    }
+
+    try {
+      // logger.debug("=============================================")
+      // logger.debug(`Rendering template: |${template}|`)
+      // logger.debug(`With context: ${JSON.stringify(context)}`)
+      // logger.debug(`Result: |${super.renderString(template, context)}|`)
+      return super.renderString(template, context)
+    } catch (error) {
+      logger.error(`Error rendering template: ${error}`)
+      logger.error(`Template: ${template}`)
+      logger.error(`Context: ${JSON.stringify(context)}`)
+      throw new Error(`Error rendering template: ${error}`)
+    }
   }
 }
